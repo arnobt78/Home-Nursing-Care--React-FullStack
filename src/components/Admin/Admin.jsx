8b4c20c2c019cf4ef7821831305c826aa21e2aea
Arnob Mahmud <arnob_t78@yahoo.com>
2025-04-15 13:43:11 +0200

    make the code clear

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import AdminLogin from "./AdminLogin";
 import AdminDashboard from "./AdminDashboard";
 
--- src/components/Admin/AdminLogin.jsx
+++ src/components/Admin/AdminLogin.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types"; // Import PropTypes for validation
+import { useState } from "react";
+import PropTypes from "prop-types";
 import axios from "axios";
 
 const AdminLogin = ({ onLogin }) => {
