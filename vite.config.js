b4bbc316dff49037db7da0cc2660310c45b22d50
Arnob Mahmud <arnob_t78@yahoo.com>
2025-05-07 15:42:59 +0200

    fix base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./", // Ensures relative paths for assets
+  base: "/", // Use absolute paths for assets
   plugins: [react()],
 });
